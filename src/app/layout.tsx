2ec63fb4710b771ba9b948bf8a0e0cf6028733ab
Igor Matheus <pessoal@igormatheus.com.br>
2023-02-28 20:00:28 -0300

    open graph support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,37 @@
 import '@/styles/globals.css'
 import { AnalyticsWrapper } from '@/components/analytics'
 import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Home',
+export const metadata: Metadata = {
+  title: 'Igor Matheus',
   description: 'My personal website',
   icons: {
     icon: [{ url: '/favicon.svg', type: 'image/svg+xml' }],
+  },
+  category: 'profile',
+  openGraph: {
+    type: 'profile',
+    siteName: 'Igor Matheus',
+    url: 'https://igormatheus.com.br/',
+    title: 'Igor Matheus',
+    description: 'My personal website',
+    images: [
+      {
+        url: 'https://igormatheus.com.br/og.png',
+        width: 800,
+        height: 600,
+        alt: 'Igor Matheus',
+      },
+    ],
+    locale: 'pt-BR',
+    countryName: 'Brazil',
+    phoneNumbers: ['+55 85 9 9968 1736'],
+    emails: ['pessoal@igormatheus.com.br'],
+    firstName: 'Igor Matheus',
+    lastName: 'Cruz de Oliveira',
   },
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { notFound } from 'next/navigation'
 import Home from '@/components/Home'
 import type { Profile, Link } from '@/types/data.types'
 
-export const dynamic = 'force-dynamic'
-export const runtime = 'experimental-edge'
+export const dynamic = 'force-dynamic',
+  runtime = 'experimental-edge'
 
 export default async function HomePage() {
   const profile = (await get('data')) as Profile
