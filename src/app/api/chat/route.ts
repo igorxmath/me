71c795518d7b18a6ac1a17a384dadd34e5f8dc37
Igor Matheus <pessoal@igormatheus.com.br>
2023-03-06 20:53:08 -0300

    .

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,8 +2,6 @@ import { ChatGPTMessage, OpenAIStreamPayload } from '@/types/chat.types'
 import { OpenAIStream } from '@/utils/OpenAIStream'
 import { NextResponse } from 'next/server'
 
-export const runtime = 'experimental-edge'
-
 export async function POST(request: Request) {
   if (!process.env.OPENAI_API_KEY || !process.env.OPENAI_ENDPOINT) {
     return new NextResponse('Server error', { status: 500 })
