069c69fa5424dfb38ff69b0d3c338bb2ca527923
Igor Matheus <pessoal@igormatheus.com.br>
2023-03-07 13:06:25 -0300

    .

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -1,8 +1,7 @@
 import SearchCard from '@/components/Tools'
 import { get } from '@vercel/edge-config'
 
-export const dynamic = 'force-dynamic',
-  runtime = 'experimental-edge'
+export const dynamic = 'force-dynamic'
 
 export default async function SearchPage() {
   const suggestions: string[] = (await get('suggestions')) as string[]
