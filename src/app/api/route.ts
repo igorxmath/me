97846e662d59d719627933cbc1a4e56f9f8e2d13
Igor Matheus <pessoal@igormatheus.com.br>
2023-03-08 08:44:13 -0300

    another test

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -4,10 +4,8 @@ import { ChatGPTMessage, ChatGPTRequest } from '@/types/chat.types'
 
 export const runtime = 'edge'
 
-export async function GET() {
-  const messages: ChatGPTMessage[] = [
-    { role: 'user', content: 'Who won the world series in 2020?' },
-  ]
+export async function POST(request: Request) {
+  const { messages }: { messages: ChatGPTMessage[] } = await request.json()
 
   const payload: ChatGPTRequest = {
     model: 'gpt-3.5-turbo',
