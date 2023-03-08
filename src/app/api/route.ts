d5ef6522d7b63727f89acbf7567af2f17c85519b
Igor Matheus <pessoal@igormatheus.com.br>
2023-03-08 08:40:46 -0300

    test

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,21 @@
 import { NextResponse } from 'next/server'
+import { OpenAIStream } from '@/utils/OpenAIStream'
+import { ChatGPTMessage, ChatGPTRequest } from '@/types/chat.types'
 
 export const runtime = 'edge'
 
 export async function GET() {
-  return new NextResponse('Hello world')
+  const messages: ChatGPTMessage[] = [
+    { role: 'user', content: 'Who won the world series in 2020?' },
+  ]
+
+  const payload: ChatGPTRequest = {
+    model: 'gpt-3.5-turbo',
+    messages,
+    stream: true,
+  }
+
+  const stream = await OpenAIStream(payload)
+
+  return new NextResponse(stream)
 }
